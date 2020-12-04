abcfb7355d910c9d4d794962e00309d7d8c8363d
okkdev <dev@stehlik.me>
2020-12-04 16:01:29 +0100

    ✨ Day4Part2

--- 4/2.scala
+++ 4/2.scala
@@ -1,6 +1,6 @@
 import scala.io.Source
 
-object Day4Part1 {
+object Day4Part2 {
   def split_file(file: String): Array[String] = {
     return "\\s\\s".r.split(Source.fromFile(file).mkString)
   }
@@ -8,10 +8,10 @@ object Day4Part1 {
   def check_passports(passports: Array[String]): Int = {
     var matches: Int = 0
     val pattern =
-      "(?=.*byr:)(?=.*iyr:)(?=.*eyr:)(?=.*hgt:)(?=.*hcl:)(?=.*ecl:)(?=.*pid:).*".r
+      raw"(?=.*byr:(19[2-9]\d|200[0-2])\b)(?=.*iyr:(201\d|2020)\b)(?=.*eyr:(202\d|2030)\b)(?=.*hgt:((1[5-8]\d|19[0-3])cm|(59|6\d|7[0-6])in))\b(?=.*hcl:#[0-f]{6}\b)(?=.*ecl:(amb|blu|brn|gr(y|n)|hzl|oth)\b)(?=.*pid:\d{9}\b).*".r
 
     for (p <- passports) {
-      if (pattern matches p.replaceAll("\n", "")) {
+      if (pattern matches p.replaceAll("\n", " ")) {
         matches += 1
       }
     }
