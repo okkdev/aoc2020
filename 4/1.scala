b475b47065eae6933c941f05e7771e12007902ab
okkdev <dev@stehlik.me>
2020-12-04 11:27:17 +0100

    ✨ Day4Part1

---
+++ 4/1.scala
@@ -0,0 +1,27 @@
+import scala.io.Source
+
+object Day4Part1 {
+  def split_file(file: String): Array[String] = {
+    return "\\s\\s".r.split(Source.fromFile(file).mkString)
+  }
+
+  def check_contents(passwords: Array[String]): Int = {
+    var matches = 0
+    val pattern =
+      "(?=.*byr:)(?=.*iyr:)(?=.*eyr:)(?=.*hgt:)(?=.*hcl:)(?=.*ecl:)(?=.*pid:).*".r
+
+    for (p <- passwords) {
+      if (pattern matches p.replaceAll("\n", "")) {
+        matches += 1
+      }
+    }
+
+    return matches
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = split_file("input.txt")
+
+    println("Valid Passwords: " + check_contents(input))
+  }
+}
