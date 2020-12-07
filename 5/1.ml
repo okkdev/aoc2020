9c0b7b77a0a868bba1a40ba968d913fa8da265b9
okkdev <stehlik.jen@gmail.com>
2020-12-07 09:01:33 +0100

    ✨ Day 5 Part 1

---
+++ 5/1.ml
@@ -0,0 +1,47 @@
+open Printf
+
+let read_lines name : string list =
+  let ic = open_in name in
+  let try_read () =
+    try Some (input_line ic) with End_of_file -> None in
+  let rec loop acc = match try_read () with
+    | Some s -> loop (s :: acc)
+    | None -> close_in ic; List.rev acc in
+  loop []
+
+let string_to_list s : char list =
+  List.init (String.length s) (String.get s)
+
+let max = function
+  | [] -> invalid_arg "empty list"
+  | x::xs -> List.fold_left max x xs
+
+let get_seat code range : int * int = 
+  let deduct (low, high) = ((high - low) / 2) + low in
+  let rec visit xs (low, high) =
+    match xs with
+    | 'F' :: rem -> visit rem (low, deduct (low, high))
+    | 'L' :: rem -> visit rem (low, deduct (low, high))
+    | 'B' :: rem -> visit rem (deduct (low, high), high)
+    | 'R' :: rem -> visit rem (deduct (low, high), high)
+    | x :: rem -> visit rem (low, high)
+    | [] -> (low, high) in
+  visit code range
+
+let get_row code : int =
+  let rowCode = string_to_list (String.sub code 0 7) in
+  let (_, row) = get_seat rowCode (0, 127) in
+  row
+
+let get_column code : int =
+  let columnCode = string_to_list (String.sub code 7 3) in
+  let (_, column) = get_seat columnCode (0, 7) in
+  column
+
+let rec id_list list = function
+  | x :: xs -> id_list (((get_row x) * 8 + (get_column x)) :: list) xs
+  | [] -> list
+
+let () = 
+  let codes = read_lines "input.txt" in
+  printf "Highest ID: %i\n" (max (id_list [] codes))
---
+++ 5/README.md
@@ -0,0 +1,5 @@
+# 🎁 [Day 5](https://adventofcode.com/2020/day/5)
+
+## OCaml
+
+Another tricky one to get into, but I like the Lisp style function calls and how you do functions in functions.
