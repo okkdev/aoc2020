f0bcea9c0342928bdbc5670dfe19606a411c34ea
okkdev <dev@stehlik.me>
2020-12-07 16:10:33 +0100

    🔥 unused sort

--- 6/2.rb
+++ 6/2.rb
@@ -1,1 +1,1 @@
-puts IO.read("input.txt").split(/\s\s/).map{ |s| s.split(/\s+/).sort.map{ |s| s.split("") } }.map { |i| i.flatten.tally.select{ |_, n| n == i.count }.count }.sum
+puts IO.read("input.txt").split(/\s\s/).map{ |s| s.split(/\s+/).map{ |s| s.split("") } }.map { |i| i.flatten.tally.select{ |_, n| n == i.count }.count }.sum
